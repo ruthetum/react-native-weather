c474199e9607f391f3dbe30b94ef8077f83efb18
Heedong Kim <ruthetum@gmail.com>
2020-05-23 19:55:26 +0900

    초기 화면 만들기(API 호출 X)
    api 값 반환되지 않았을 때 기본화면 만들기

---
+++ App.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+// 컴포넌트를 사용할 때는 import 시켜야함
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component{
+  state = {
+    isLoaded: false
+  }
+  render() {
+    const { isLoaded } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoaded ? null :
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the Weather</Text>
+          </View>}
+      </View>
+    );
+  }
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // flexDirection:'row',      // 가로 방향 배치
+    // justifyContent:'center',  // 세로 가운데
+    // alignItems:'center'       // 가로 가운데
+  },
+  loading: {
+    flex:1,
+    backgroundColor: '#FDF6AA',
+    justifyContent:'flex-end',
+    alignItems: 'center'
+  },
+  loadingText: {
+    fontSize:38,
+    marginBottom:150
+  }
+});
